c727d48124bd73d8f606029cff53d7b485035257
cristianutc <zyzzgutierrez@gmail.com>
2022-09-24 21:36:46 -0500

    nuevo

---
+++ 02holamundo/holamundo.java
@@ -0,0 +1,23 @@
+//esto es un comentario
+/*
+ * este comentario nos sirve para poder
+ * tener varias lineas comentadas del codigo
+ * todo programa en java, maneja
+ * clases, una clase es un objeto del mundo real
+ */
+class holamundo{
+//cuerpo de programa
+/*
+ * aqui es donde nosotros
+ * codificamos todos los elementos
+ * del programa, funciones,
+ * estructura, variables y metodos
+ * toda clase debe tener un
+ * metodo principal
+ */
+
+ public static void main(String[]arg){
+    System.out.println("holamundo");
+ }
+
+}
